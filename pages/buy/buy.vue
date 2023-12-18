28ba150f6f8e788a15507379714bb640df5a35c0
heboping <heboping2022@163.com>
2023-12-18 22:48:38 +0800

    feat： 补充提交2

---
+++ pages/buy/buy.vue
@@ -0,0 +1,44 @@
+<template>
+  <view style="margin-top: 40px">
+    <b><h2 style="text-align: center; ">购买记录</h2></b>
+    <view style="width: 100%;height: 40px"></view>
+
+    <u-search
+        height="38px"
+        placeholder="搜索"></u-search>
+
+    <view class="bottom"></view>
+
+    <view class="bottom1"></view>
+  </view>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+
+<style>
+.bottom {
+  width: 300px;
+  height: 100px;
+  border: 1px solid #808080;
+  margin: 50px 30px;
+}
+.bottom1 {
+  width: 300px;
+  height: 250px;
+  border: 1px solid #808080;
+  margin: 60px 30px;
+}
+
+</style>
---
+++ pages/product/product.vue
@@ -0,0 +1,36 @@
+<template>
+	<view style="margin-top: 40px">
+    <b><h2 style="text-align: center; ">管理产品</h2></b>
+    <view style="width: 100%;height: 40px"></view>
+
+    <u-search
+        height="38px"
+        placeholder="搜索"></u-search>
+
+    <view class="bottom"></view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+
+<style>
+.bottom {
+  width: 300px;
+  height: 100px;
+  border: 1px solid #808080;
+  margin: 50px 30px;
+}
+
+</style>
