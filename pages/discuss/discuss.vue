8bd9f8710e1eacf51ea63a1727035ad7f3572e26
heboping <heboping2022@163.com>
2023-12-18 23:11:05 +0800

    feat： 补充提交3

---
+++ pages/discuss/discuss.vue
@@ -0,0 +1,29 @@
+<template>
+	<view style="margin-top: 40px">
+
+    <b><h2 style="text-align: center; ">xxx股票讨论区</h2></b>
+    <view style="width: 100%;height: 40px"></view>
+    <u-search
+        height="38px"
+        placeholder="搜索"></u-search>
+
+
+  </view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
